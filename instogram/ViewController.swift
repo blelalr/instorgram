1fe26f22dc2e9a1c598653cf251e9140d2a3bf99
Eros <blelarg@gmail.com>
2017-03-27 20:01:51 +0800

    init

---
+++ instogram/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  instogram
+//
+//  Created by Eros on 2017/3/27.
+//  Copyright © 2017年 Eros. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
